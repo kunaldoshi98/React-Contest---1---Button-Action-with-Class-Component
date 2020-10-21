ad7d2660afbcfe487db3ccbb385810e6a15db6c9
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-10-21 20:29:13 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,14 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    return <div id="main">{<button id="click">Click</button>}</div>;
+  }
 }
 
-
 export default App;
-
