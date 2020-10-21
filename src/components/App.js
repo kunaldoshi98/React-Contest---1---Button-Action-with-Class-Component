b8a94b24d81704a90e74c79d06adeb6a8d1d0c10
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-10-21 21:17:03 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,27 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      render: false
+    };
     this.sayHello = this.sayHello.bind(this);
   }
   sayHello() {
-    return (
-      <p>
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
-    );
+    this.setState({ render: !this.state.render });
   }
 
   render() {
     return (
       <div id="main">
-        {
-          <button id="click" onClick={this.sayHello}>
-            Click
-          </button>
-        }
+        <button id="click" onClick={this.sayHello}>
+          Click
+        </button>
+        {this.state.render && (
+          <p>
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
       </div>
     );
   }
